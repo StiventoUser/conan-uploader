3018528266cec622b432ffa8783cf93cd94edbd2
v.krapivin <v.krapivin@avantize.com>
2018-06-21 17:14:17 +0300

    Added colored output.

--- test_and_upload.py
+++ test_and_upload.py
@@ -4,6 +4,7 @@ import pathlib
 import json
 import re
 from subprocess import run
+from colorama import init, Fore, Back, Style
 
 conanWinSettings = [
     {'build_type': 'Debug', 'arch': 'x86_64', 'compiler': 'Visual Studio', 'compiler.runtime': 'MTd', 'compiler.version': '14', 'os': 'Windows'},
@@ -31,6 +32,17 @@ def delete_folder(pth) :
     pth.rmdir()
 
 
+def install_repo(package):
+    match = re.search(':\/\/.+?\/.+?\/(.+)', package['url'])
+    repo_path = os.path.join(os.getcwd(), match.group(1))
+
+    delete_folder(pathlib.Path(repo_path))
+
+    run('git clone --recurse-submodules {}'.format(package['url']), check=True)
+
+    return repo_path
+
+
 def get_package_info(package_path):
     with open(os.path.join(package_path, 'conanfile.py')) as f:
         file_data = f.read()
@@ -54,47 +66,46 @@ def create_package(package_info):
     for settings in conanWinSettings:
         params = ['-s {}=\"{}\"'.format(s, v) for (s, v) in settings.items()]
 
-        command = ' '.join(['conan', 'create',  '.', '{}/{}'.format(owner, thread), *params, '--build=' + name, '--build=missing'])
+        command = ' '.join(
+            ['conan', 'create',  '.', '{}/{}'.format(owner, thread), *params, '--build=' + name, '--build=missing'])
 
         print("Executing: {}".format(command))
-        run(command, check=True)
+        run(command, check=False)
 
 
-def upload_package(package_info, org_name, user_name):
+def upload_package(package_info, primary_name, secondary_name):
     command = 'conan upload {}/{}@{}/{} -r {} --all'\
         .format(package_info['name'], package_info['version'], package_info['owner'], package_info['thread'], '{}' )
 
-    if user_name:
-        run(command.format(user_name))
+    if secondary_name:
+        run(command.format(secondary_name))
 
-    run(command.format(org_name))
+    run(command.format(primary_name))
 
 
 if __name__ == '__main__':
+    init(autoreset=True)
+
     with open('packages,json') as f:
         config = json.load(f)
 
-    org_name = config["organization"]
+    primary_name = config["primary"]
 
-    if not org_name:
-        raise Exception("Organization is required")
+    if not primary_name:
+        raise Exception("Primary remote is required")
 
     for package in config['packages']:
-        match = re.search(':\/\/.+?\/.+?\/(.+)', package['url'])
-        repo_path = os.path.join(os.getcwd(), match.group(1))
+        print(Fore.CYAN + "-------------------------Building {}".format(package['url']))
 
-        delete_folder(pathlib.Path(repo_path))
-
-        run('git clone --recurse-submodules {}'.format(package['url']), check=True)
+        repo_path = install_repo(package)
 
         package_info = get_package_info(repo_path)
+
         package_info['thread'] = package['thread']
+        secondary_name = package['secondary'] if 'secondary' in package else None
+        package_info['owner'] = secondary_name if secondary_name else primary_name
 
-        user_name = package['user'] if 'user' in package else None
-
-        package_info['owner'] = user_name if user_name else org_name
-
-        print("Package info: {}".format(package_info))
+        print(Fore.CYAN + "Package info: {}".format(package_info))
 
         old_dir = os.getcwd()
         try:
@@ -104,4 +115,6 @@ if __name__ == '__main__':
         finally:
             os.chdir(old_dir)
 
-        upload_package(package_info, org_name, user_name)
+        upload_package(package_info, primary_name, secondary_name)
+
+        print(Fore.GREEN + "-------------------------Built. {}".format(package_info['name']))
